df6a2e618abed5811ce1d9e557285066c4843a73
Martin Toman <whomenope@outlook.com>
2020-02-18 23:22:12 +0100

    Add example program + compilation for STM32F767ZI

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,35 @@
 module Main where
 
+import Atom.Language.Atom
+import STM32F767ZI (compileProgram)
+
 main :: IO ()
-main = someFunc
+main = compileProgram program
+
+-- | An example design that computes the greatest common divisor.
+program :: Atom ()
+program = do
+
+  -- External reference to value A.
+  let a = word32' "a"
+
+  -- External reference to value B.
+  let b = word32' "b"
+
+  -- The external running flag.
+  let running = bool' "running"
+
+  -- A rule to modify A.
+  atom "a_minus_b" $ do
+    cond $ value a >. value b
+    a <== value a - value b
+
+  -- A rule to modify B.
+  atom "b_minus_a" $ do
+    cond $ value b >. value a
+    b <== value b - value a
+
+  -- A rule to clear the running flag.
+  atom "stop" $ do
+    cond $ value a ==. value b
+    running <== false
---
+++ src/STM32F767ZI.hs
@@ -0,0 +1,42 @@
+module STM32F767ZI
+  ( compileProgram
+  ) where
+
+import Atom.Language.Atom
+
+-- | Invoke the atom compiler.
+compileProgram :: Atom () -> IO ()
+compileProgram program = do
+  let loopName = "main_loop"
+  (schedule, _, _, _, _) <- compile loopName defaults { cCode = prePostCode loopName } program
+  putStrLn $ reportSchedule schedule
+
+prePostCode :: String -> [Name] -> [Name] -> [(Name, Type)] -> (String, String)
+prePostCode loopName _ _ _ =
+  ( unlines
+    [ "#include <stdlib.h>"
+    , "#include <stdio.h>"
+    , "unsigned long int a;"
+    , "unsigned long int b;"
+    , "unsigned long int x;"
+    , "unsigned char running = 1;"
+    ]
+  , unlines
+    [ "int main(int argc, char* argv[]) {"
+    , "  if (argc < 3) {"
+    , "    printf(\"usage: gcd <num1> <num2>\\n\");"
+    , "  }"
+    , "  else {"
+    , "    a = atoi(argv[1]);"
+    , "    b = atoi(argv[2]);"
+    , "    printf(\"Computing the GCD of %lu and %lu...\\n\", a, b);"
+    , "    while(running) {"
+    , "      " ++ loopName ++ "();"
+    , "      printf(\"iteration:  a = %lu  b = %lu\\n\", a, b);"
+    , "    }"
+    , "    printf(\"GCD result: %lu\\n\", a);"
+    , "  }"
+    , "  return 0;"
+    , "}"
+    ]
+  )
