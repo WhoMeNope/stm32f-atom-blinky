592d570f93e8af36cc2ebf0ddce64393b3f72719
Martin Toman <whomenope@outlook.com>
2020-02-19 00:28:48 +0100

    Use QuasiQuotes for inline C

--- src/STM32F767ZI.hs
+++ src/STM32F767ZI.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module STM32F767ZI
   ( compileProgram
   ) where
 
 import Atom.Language.Atom
+import NeatInterpolation (trimming)
+import Data.Text (pack, unpack)
 
 -- | Invoke the atom compiler.
 compileProgram :: Atom () -> IO ()
@@ -13,30 +18,33 @@ compileProgram program = do
 
 prePostCode :: String -> [Name] -> [Name] -> [(Name, Type)] -> (String, String)
 prePostCode loopName _ _ _ =
-  ( unlines
-    [ "#include <stdlib.h>"
-    , "#include <stdio.h>"
-    , "unsigned long int a;"
-    , "unsigned long int b;"
-    , "unsigned long int x;"
-    , "unsigned char running = 1;"
-    ]
-  , unlines
-    [ "int main(int argc, char* argv[]) {"
-    , "  if (argc < 3) {"
-    , "    printf(\"usage: gcd <num1> <num2>\\n\");"
-    , "  }"
-    , "  else {"
-    , "    a = atoi(argv[1]);"
-    , "    b = atoi(argv[2]);"
-    , "    printf(\"Computing the GCD of %lu and %lu...\\n\", a, b);"
-    , "    while(running) {"
-    , "      " ++ loopName ++ "();"
-    , "      printf(\"iteration:  a = %lu  b = %lu\\n\", a, b);"
-    , "    }"
-    , "    printf(\"GCD result: %lu\\n\", a);"
-    , "  }"
-    , "  return 0;"
-    , "}"
-    ]
+  let loopName' = pack loopName
+   in
+  ( unpack $ [trimming|
+      #include <stdlib.h>
+      #include <stdio.h>
+
+      unsigned long int a;
+      unsigned long int b;
+      unsigned long int x;
+      unsigned char running = 1;
+    |]
+  , unpack $ [trimming|
+      int main(int argc, char* argv[]) {
+        if (argc < 3) {
+          printf("usage: gcd <num1> <num2>\n");
+        }
+        else {
+          a = atoi(argv[1]);
+          b = atoi(argv[2]);
+          printf("Computing the GCD of %lu and %lu...\n\", a, b);"
+          while(running) {
+            ${loopName'}();
+            printf("iteration:  a = %lu  b = %lu\n\", a, b);
+          }
+          printf("GCD result: %lu\n\", a);
+        }
+        return 0;
+      }
+    |]
   )
