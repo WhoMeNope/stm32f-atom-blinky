6783d2079c5ffe25293f4a9fb80c0410bcc19b83
Martin Toman <whomenope@outlook.com>
2020-02-19 01:10:58 +0100

    Define blinky program

--- app/Main.hs
+++ app/Main.hs
@@ -6,30 +6,9 @@ import STM32F767ZI (compileProgram)
 main :: IO ()
 main = compileProgram "blinky" program
 
--- | An example design that computes the greatest common divisor.
 program :: Atom ()
 program = do
+  let led1 = led' "led1" False
 
-  -- External reference to value A.
-  let a = word32' "a"
-
-  -- External reference to value B.
-  let b = word32' "b"
-
-  -- The external running flag.
-  let running = bool' "running"
-
-  -- A rule to modify A.
-  atom "a_minus_b" $ do
-    cond $ value a >. value b
-    a <== value a - value b
-
-  -- A rule to modify B.
-  atom "b_minus_a" $ do
-    cond $ value b >. value a
-    b <== value b - value a
-
-  -- A rule to clear the running flag.
-  atom "stop" $ do
-    cond $ value a ==. value b
-    running <== false
+  period 1000 $ atom "blink" $ do
+    led1 <== (not $ value led1)
